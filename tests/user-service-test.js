4e31987ffb4ee6a55b53752875b2f1d404a5f15c
IhorSokolyk <ihorsokolyk1993@gmail.com>
2017-07-12 22:08:34 +0300

    Added more tests

--- tests/user-service-test.js
+++ tests/user-service-test.js
@@ -9,9 +9,18 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+let user = {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john_doe@gmail.com",
+    password: "11111111"
+};
+let userId = undefined;
+let accessToken = undefined;
+
 describe('UserService', () => {
 
-    beforeEach((done) => {
+    before((done) => {
         User.remove({}, (err) => {
             done();
         });
@@ -19,12 +28,6 @@ describe('UserService', () => {
 
     describe('/POST User', () => {
         it('it should create new user', (done) => {
-            let user = {
-                firstName: "John",
-                lastName: "Doe",
-                email: "john_doe@gmail.com",
-                password: "11111111"
-            };
             chai.request(server)
                 .post('/user/save')
                 .send(user)
@@ -32,6 +35,7 @@ describe('UserService', () => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
                     res.body.should.have.property('_id');
+                    userId = res.body._id;
                     done();
                 });
         });
@@ -49,6 +53,34 @@ describe('UserService', () => {
                 .end((err, res) => {
                     res.should.have.status(401);
                     res.body.should.have.property('error');
+                    done();
+                });
+        });
+        it('it should login user and return userId and accessToken', (done) => {
+            chai.request(server)
+                .post('/user/login')
+                .send(user)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('userId');
+                    res.body.should.have.property('accessToken');
+                    accessToken = res.body.accessToken;
+                    done();
+                });
+        });
+    });
+
+    describe('/GET User', () => {
+        it('it should get user by id using token authentication', (done) => {
+            chai.request(server)
+                .get('/user/get/' + userId)
+                .set('authorization', accessToken)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('email');
+                    res.body.should.have.property('firstName');
+                    res.body.should.have.property('lastName');
+                    res.body.should.not.have.property('password');
                     done();
                 });
         });
