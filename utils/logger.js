902ee2089312e5ae52ab4075158957206540f88d
Ihor Sokolyk <ihorsokolyk1993@gmail.com>
2017-07-13 09:55:21 +0300

    Improved logger

--- utils/logger.js
+++ utils/logger.js
@@ -2,12 +2,18 @@ let winston = require('winston');
 
 let logger = new (winston.Logger)({
     transports: [
-        new (winston.transports.Console)({level: 'debug', handleExceptions: true}),
+        new (winston.transports.Console)({
+            level: 'debug',
+            handleExceptions: true,
+            humanReadableUnhandledException: true,
+            colorize: true}),
 
         new (winston.transports.File)({
             filename: 'server_logs.log',
             level: 'debug',
-            handleExceptions: true
+            handleExceptions: true,
+            maxsize: 10000,
+            maxFiles: 10
         })
     ]
 });
